dee72990c04a0f26334d955dfab1a77c07d81a43
Finn O'Keeffe <okeeffefinn@gmail.com>
2025-04-02 21:41:56 +1300

    Added some autocompletion with nvim-cmp

--- nvim/lua/finnokeeffe/packer.lua
+++ nvim/lua/finnokeeffe/packer.lua
@@ -18,6 +18,13 @@ return require('packer').startup(function(use)
   use {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig"
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip"
   }
 end)
