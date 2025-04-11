75d1a701399ebd5255d9e6740f2edc7d2081a9da
Finn O'Keeffe <okeeffefinn@gmail.com>
2025-04-11 20:02:43 +1200

    Simplified code a little - learned I can't just turn off my nvim colourscheme, it won't use the terminal's by default

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -6,7 +6,4 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
-
-ColorMyPencils()
+ColorMyPencils("gruvbox")
