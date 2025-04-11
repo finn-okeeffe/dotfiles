0246be4fd670ca662f0940fdab9af83ec138c36a
Finn O'Keeffe <finn.okeeffe@scarlatti.co.nz>
2025-04-11 12:07:17 +1200

    Added resurrect plugin, but doesn't seem to work well at resurrecting neovim at the moment

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,12 @@
 local wezterm = require("wezterm")
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
 local config = wezterm.config_builder()
 
 wezterm.on('toggle-background', function(window, pane)
     local overrides = window.get_config_overrides() or {}
     if not overrides.window_backgroud_image then
         overrides.window_backgroud_image = "/home/finnokeeffe/git/dotfiles/wezterm/assets/gruvbox-wallpaper-cave.png"
-    else
+
         overrides.window_backgroud_image = nil
     end
     window:set_config_overrides(overrides)
@@ -50,6 +51,34 @@ config.keys = {
         }
     },
     {
+        key="UpArrow",
+        mods="ALT|SHIFT",
+        action=wezterm.action.AdjustPaneSize {
+            "Up", 1
+        }
+    },
+    {
+        key="DownArrow",
+        mods="ALT|SHIFT",
+        action=wezterm.action.AdjustPaneSize {
+            "Down", 1
+        }
+    },
+    {
+        key="RightArrow",
+        mods="ALT|SHIFT",
+        action=wezterm.action.AdjustPaneSize {
+            "Right", 1
+        }
+    },
+    {
+        key="LeftArrow",
+        mods="ALT|SHIFT",
+        action=wezterm.action.AdjustPaneSize {
+            "Left", 1
+        }
+    },
+    {
         key="LeftArrow",
         mods="ALT",
         action=wezterm.action.ActivatePaneDirection "Left",
@@ -88,8 +117,58 @@ config.keys = {
         key="F11",
         action=wezterm.action.ToggleFullScreen,
     },
+    {
+        key = "w",
+        mods = "ALT",
+        action = wezterm.action_callback(function(win, pane)
+            resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+          end),
+    },
+    {
+        key = "W",
+        mods = "ALT",
+        action = resurrect.window_state.save_window_action(),
+    },
+    {
+        key = "T",
+        mods = "ALT",
+        action = resurrect.tab_state.save_tab_action(),
+    },
+    {
+        key = "s",
+        mods = "ALT",
+        action = wezterm.action_callback(function(win, pane)
+            resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+            resurrect.window_state.save_window_action()
+        end),
+    },
+    { -- loading states with fuzzy find
+        key = "r",
+        mods = "ALT",
+        action = wezterm.action_callback(function(win, pane)
+            resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
+                local type = string.match(id, "^([^/]+)") -- match before '/'
+                id = string.match(id, "([^/]+)$") -- match after '/'
+                id = string.match(id, "(.+)%..+$") -- remove file extention
+                local opts = {
+                    relative = true,
+                    restore_text = true,
+                    on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+                }
+                if type == "workspace" then
+                    local state = resurrect.state_manager.load_state(id, "workspace")
+                    resurrect.workspace_state.restore_workspace(state, opts)
+                elseif type == "window" then
+                    local state = resurrect.state_manager.load_state(id, "window")
+                    resurrect.window_state.restore_window(pane:window(), state, opts)
+                elseif type == "tab" then
+                    local state = resurrect.state_manager.load_state(id, "tab")
+                    resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+                end
+            end)
+        end),
+    },
 }
-
 -- Remove top bars
 config.window_decorations = "RESIZE" -- disable title bar
 config.hide_tab_bar_if_only_one_tab = true
