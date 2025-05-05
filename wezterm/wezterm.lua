29dfbf39b24a2a75b37e1e19fc426ba336f11a3f
Finn O'Keeffe <finn.okeeffe@scarlatti.co.nz>
2025-05-06 10:57:56 +1200

    Switched default windows shell to wsl

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -268,7 +268,7 @@ end)
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     -- Launch in powershell
-    config.default_prog = {'powershell.exe'}
+    config.default_prog = {'wsl.exe'}
 end
 
 
