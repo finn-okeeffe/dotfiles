e5fac11abdbad9da32ed5e62b65bc85cebd0beb6
Finn O'Keeffe <finn.okeeffe@scarlatti.co.nz>
2026-05-21 13:52:53 +1200

    Fixes to nvim config and updating pinned plugin versions for nvim 0.12.x

--- nvim/lua/finnokeeffe/lazy.lua
+++ nvim/lua/finnokeeffe/lazy.lua
@@ -26,11 +26,53 @@ require("lazy").setup({
   spec = {
 	  {'nvim-lua/plenary.nvim'},
 	  {
-		  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		  'nvim-telescope/telescope.nvim', branch = 'master',
 		  dependencies = {'nvim-lua/plenary.nvim'}
 	  },
 	  { "ellisonleao/gruvbox.nvim" },
-	  { 'nvim-treesitter/nvim-treesitter', branch='master', lazy=false, build=':TSUpdate'},
+	  {
+		  'nvim-treesitter/nvim-treesitter',
+		  branch = 'main',
+		  lazy = false,
+		  build = ':TSUpdate',
+		  config = function()
+			  local languages = {
+				  "powershell",
+				  "bash",
+				  "python",
+				  "c",
+				  "lua",
+				  "vim",
+				  "vimdoc",
+				  "query",
+				  "markdown",
+				  "markdown_inline",
+			  }
+
+			  require("nvim-treesitter").setup()
+			  require("nvim-treesitter").install(languages)
+
+			  local group = vim.api.nvim_create_augroup("nvim-treesitter-start", { clear = true })
+
+			  local function start_highlight(bufnr)
+				  bufnr = bufnr or vim.api.nvim_get_current_buf()
+				  if vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].filetype ~= "" then
+					  pcall(vim.treesitter.start, bufnr)
+				  end
+			  end
+
+			  vim.api.nvim_create_autocmd("FileType", {
+				  group = group,
+				  callback = function(args)
+					  start_highlight(args.buf)
+				  end,
+			  })
+
+			  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+				  start_highlight(bufnr)
+			  end
+		  end,
+	  },
 	  {'mbbill/undotree'},
 	  {"williamboman/mason.nvim"},
 	  {"williamboman/mason-lspconfig.nvim"},
